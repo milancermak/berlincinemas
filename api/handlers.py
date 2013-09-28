898d2ffbd0bc8f725d0707d55a3f128c248537fa
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-09-28 18:21:44 +0200

    Endpoint /cinemas/ returns a list of all available cinemas in the DB.

--- api/handlers.py
+++ api/handlers.py
@@ -12,25 +12,20 @@ from flask import Flask
 from flask import make_response
 app = Flask(__name__)
 
-from api import cinema
-
-@app.route("/cinemas/berlin/")
+@app.route("/movies/")
 def berlin():
     response = make_response(json.dumps(cinema.list()))
     response.headers["Content-Type"] = "application/json"
     return response
 
-@app.route("/latitude/<float:latitude>/longitude/<float:longitude>")
-def near_cinema(latitude,longitude):
-    response = make_response( \
-    json.dumps(cinema.find_by_latitude_longitude(latitude, longitude)))
-    response.headers["Content-Type"] = "application/json"
-    return response
-
-@app.route("/cinemas/<cinema_id>")
-def get_cinema(cinema_id):
+@app.route("/cinemas/")
+def get_cinema():
     import api
-    c = api.connection.Cinema.one({'name': cinema_id})
-    response = make_response(c.to_json())
+    result = []
+    cinemas = api.connection.berlincinemas.cinemas.find()
+    for kino in cinemas:
+        kino.pop('_id')
+        result.append(kino)
+    response = make_response(json.dumps(result))
     response.headers["Content-Type"] = "application/json"
     return response
