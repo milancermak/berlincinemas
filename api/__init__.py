03312a79173af6e200bfc5ee98971a6dccfab6d3
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-10-21 15:50:53 +0200

    Path for config file relative to the file that tries to read it.

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 from mongokit import Connection
+from os import path
+from inspect import getfile, currentframe
 
 app = Flask("API")
 #app.config.from_object("config")
@@ -10,7 +12,8 @@ import configparser
 
 # TODO handle errors!
 config = configparser.ConfigParser()
-config.read( 'config.py' )
+config.read(path.join(path.dirname(path.abspath(getfile(currentframe()))), '../config.py'))
+
 connection = Connection( config['DATABASE']['MONGODB_HOST'], int(config['DATABASE']['MONGODB_PORT']) )
 
 # After db connection and flask config, flask.routes enabled.
