282e6a5777b1baa3b914cc8425324ef88a91ffb7
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-10-21 16:01:35 +0200

    Less expensive way to get the path (@milancermak said)

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,6 @@
 from flask import Flask
 from mongokit import Connection
 from os import path
-from inspect import getfile, currentframe
 
 app = Flask("API")
 #app.config.from_object("config")
@@ -12,7 +11,7 @@ import configparser
 
 # TODO handle errors!
 config = configparser.ConfigParser()
-config.read(path.join(path.dirname(path.abspath(getfile(currentframe()))), '../config.py'))
+config.read(path.join(path.abspath(path.dirname(__file__)), "..", "config.py"))
 
 connection = Connection( config['DATABASE']['MONGODB_HOST'], int(config['DATABASE']['MONGODB_PORT']) )
 
