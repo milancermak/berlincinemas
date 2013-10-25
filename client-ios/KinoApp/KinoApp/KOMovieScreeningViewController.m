80a8f55bddbd307195adffdd43401fdd3abd9de7
Milan Cermak <milan.cermak@gmail.com>
2013-10-25 18:05:39 +0200

    Adding a GMap to the detail view (WIP)

--- client-ios/KinoApp/KinoApp/KOMovieScreeningViewController.m
+++ client-ios/KinoApp/KinoApp/KOMovieScreeningViewController.m
@@ -6,10 +6,20 @@
 //  Copyright (c) 2013 Milan Cermak. All rights reserved.
 //
 
+#import <GoogleMaps/GoogleMaps.h>
 #import "KOModels.h"
 #import "KOMovieScreeningViewController.h"
+#import "Masonry.h"
+
+const NSUInteger initialMapZoomLevel = 10;
+const CGFloat goldenRatio = 1.61803398875;
 
 @interface KOMovieScreeningViewController ()
+
+@property (nonatomic, strong) GMSMapView *mapView;
+@property (nonatomic, strong, readonly) KOMovie *movie;
+
+- (void)setUpAutolayoutConstraints;
 
 @end
 
@@ -18,6 +28,7 @@
 - (instancetype)initWithMovie:(KOMovie *)movie {
     self = [super initWithNibName:nil bundle:nil];
     if (self) {
+        _movie = movie;
         self.title = movie.title;
     }
     return self;
@@ -25,6 +36,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.mapView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setUpAutolayoutConstraints];
+}
+
+#pragma mark - Private
+
+- (void)setUpAutolayoutConstraints {
+    CGFloat topOffset = self.topLayoutGuide.length;
+
+    [self.mapView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).offset(topOffset);
+        make.right.equalTo(self.view.mas_right);
+        make.left.equalTo(self.view.mas_left);
+        make.height.equalTo(self.view.mas_height).multipliedBy(1/goldenRatio);
+    }];
+}
+
+#pragma mark - Subviews
+
+- (GMSMapView *)mapView {
+    if (!_mapView) {
+        GMSCameraPosition *camera = [GMSCameraPosition cameraWithTarget:self.movie.cinema.location.coordinate
+                                                                   zoom:initialMapZoomLevel];
+        _mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+        _mapView.buildingsEnabled = NO;
+        _mapView.myLocationEnabled = YES;
+    }
+    return _mapView;
 }
 
 @end
