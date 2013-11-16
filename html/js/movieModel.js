fdae4d18f6499277a2cf23345ef27d749e13608c
Josh Wilson <joshuef@gmail.com>
2013-11-16 13:40:21 +0100

    Updated the site with some things that are maybe okay. Also uses the /today endpoint. :BOOM:

--- html/js/movieModel.js
+++ html/js/movieModel.js
@@ -9,14 +9,33 @@ var Movie = Backbone.Model.extend({
         var showTime =  moment( Date.parse( this.get('date') ) );
 
 
+        // Add a list of kinos
         var kinoObj = {};
 
         kinoObj[ this.get( 'cinema' ) ] = [ showTime ];  
         
-
         this.set( 'kinos', kinoObj );
         this.set( 'showTimes', [ showTime ] );
 
+        this.setOriginalLanguage();
+
    },
 
+   setOriginalLanguage : function( )
+   {
+        var self = this;
+
+
+        var omuPosition = self.get( 'title' ).search( 'OmU' );
+        var ovPosition = self.get( 'title' ).search( 'OV' );
+
+        if( omuPosition > 0 || ovPosition > 0 )
+        {
+            self.set( 'original', true )
+        }
+        else
+        {
+            self.set( 'original', false )
+        }
+   }
 });
